927481eef0f797939a0c98b5628fa43ae234fce2
Mark R Pothecary <markpothers@hotmail.com>
2019-04-25 15:53:41 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, { Component } from 'react'
+
+export default class App extends Component {
+
+    state = {
+        inSpace: {}
+    }
+
+    render() {
+        console.log(this.state)
+        return (
+            <div>
+            </div>
+        )
+    }
+
+    componentDidMount(){
+        fetch('http://api.open-notify.org/astros.json')
+            .then(res => res.json())
+            .then(returnedData => {
+                this.setState({inSpace: returnedData})
+            })
+    }
+
+}
